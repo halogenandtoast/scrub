3af3ab3d79d59c34153c997557c848ceb9a7430d
Matt Mongeau <matt.mongeau@digitalscientists.com>
2009-12-22 17:29:26 -0500

    Initial Commit

---
+++ Rakefile
@@ -0,0 +1,13 @@
+begin
+  require 'jeweler'
+  Jeweler::Tasks.new do |gemspec|
+    gemspec.name = "scrub"
+    gemspec.summary = "Scrub files with ease and elbow grease"
+    gemspec.description = "Scrub files with ease and elbow grease"
+    gemspec.email = "matt@toastyapps.com"
+    gemspec.homepage = "http://github.com/toastyapps/scrub"
+    gemspec.authors = ["Matthew Mongeau"]
+  end
+rescue LoadError
+  puts "Jeweler not available. Install it with: sudo gem install jeweler"
+end
---
+++ lib/scrub.rb
@@ -0,0 +1,44 @@
+module Scrub
+  class Scrubber
+    def initialize(&block)
+      config(&block) if block_given?
+    end
+  
+    def config
+      @config = Scrubber::Config.new
+      yield @config
+    end
+  
+    def crawl(data, splitter = /\n/)
+      lines = data.split(splitter)
+      while(line = lines.shift)
+        line.strip!
+        @config.matchers.each do |matcher, prok|
+          prok.call(line) if line =~ matcher
+        end
+        @config.between_matchers.each do |matcher, options|
+          if line =~ matcher
+            end_matcher, prok, conditions = options
+            next_line = nil
+            while(next_line = lines.shift) && (next_line !~ end_matcher)
+              prok.call(next_line) if next_line =~ conditions[:when]
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  class Config
+    attr_accessor :matchers, :between_matchers
+    def initialize
+      @matchers, @between_matchers = {}, {}
+    end
+    def when matcher, &block
+      @matchers[matcher] = block
+    end
+    def between beginning_matcher, end_matcher, options = {}, &block
+      @between_matchers[beginning_matcher] = [end_matcher, block, {:when => /.*?/}.merge(options)]
+    end
+  end
+end
